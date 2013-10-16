928cf100231c13d7eda2eab7a3f8e3b5b8c92c31
Marcus Johansson <marcushenrik@gmail.com>
2013-10-16 18:13:28 +0200

    Added a TODO-task to ScheduleItem

--- android/src/se/mah/k3/schedule/ScheduleItem.java
+++ android/src/se/mah/k3/schedule/ScheduleItem.java
@@ -2,6 +2,18 @@ package se.mah.k3.schedule;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import net.fortuna.ical4j.model.component.VEvent;
+/*
+ *  TODO: 
+ *  Kronox gives us a very oddly formatted SUMMARY field. We do not want to show this as is.
+ *  So we need to figure out what information we can extract here and categorize it.
+ *  You get the summary by the command v.getSummary().getValue() in the constructor below
+ *  A typical string can look like:
+ *
+ *  Coursegrp: KD330A-20132-62311- Sign: K3LARA Description: Project room Activity type: Okänd
+ *  Programme: VGSJU13h VGSJU13h1 VGSJU13h2 Coursegrp: OM113A-20132-OM113-D16 Sign: HSANMOS Description: Övning Injektionsgivning VP. 16:1 Activity type: Okänd
+ *  
+ *  Can we make it presentable?
+ */
 public class ScheduleItem {
 	SimpleDateFormat time_format = new SimpleDateFormat("HH:mm", Locale.US);
 	SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm",
